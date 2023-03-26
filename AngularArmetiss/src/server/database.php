dc4d4740a3160e5f4febbc28044559f59c06bae3
celikesad <la218697@student.helha.be>
2023-03-26 17:45:28 +0200

    Connexion a la bdd

--- AngularArmetiss/src/server/database.php
+++ AngularArmetiss/src/server/database.php
@@ -4,13 +4,13 @@
   $db_password='*j#$!^C5@7K879C583s6';
   $db_name='armecaisse';
 
-  function connect(){
+  function connect($db_host,$db_username,$db_name,$db_password){
     try{
       $connexion=new PDO("mysql:host=$db_host;dbname=$db_name", $db_username, $db_password);
       $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       return $connexion;
     }catch(PDOException $e){
-      console.log('Echec de la connexion : '.$e->getMessage());
+      print('Echec de la connexion : '.$e->getMessage());
     }
   }
 

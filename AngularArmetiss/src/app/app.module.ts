ecbe72e83a68b912f72ef5d85e3ef1fafb643f2d
Pusic Filip <la216187@student.helha.be>
2023-04-05 19:03:42 +0200

    Ajout component carte, header et création de la page d'accueil

--- AngularArmetiss/src/app/app.module.ts
+++ AngularArmetiss/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './gestion-utilisateur/user.module';
+import { ContainerCardsComponent } from './container-cards/container-cards.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileDropdownComponent } from './profile-dropdown/profile-dropdown.component';
+import { CardPageComponent } from './card-page/card-page.component';
+import { UserFormComponent } from './gestion-utilisateur/user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerCardsComponent,
+    HeaderComponent,
+    ProfileDropdownComponent,
+    CardPageComponent,
+    
   ],
   imports: [
     BrowserModule,

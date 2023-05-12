f2285bdbb24ee59868c0c085cc7d77ae63c7a52d
Falconi Franco <100429078+la216677@users.noreply.github.com>
2023-05-12 10:59:19 +0200

    Ajout bouton retour

--- AngularArmetiss/src/app/header/header.component.ts
+++ AngularArmetiss/src/app/header/header.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HeaderService } from './header.service';
 import { CookieService } from 'ngx-cookie-service';
 import { Subscription } from 'rxjs';
+import { Location } from '@angular/common';
+import { NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -11,9 +14,22 @@ import { Subscription } from 'rxjs';
 })
 export class HeaderComponent implements OnInit {
 
+  datas!: Subscription
   data!: Subscription;
   connected!: boolean;
-  constructor(private router:Router, private headerService: HeaderService, private cookieService: CookieService){}
+  flagGoBackArrow: boolean = false;
+
+  constructor(private router:Router, private headerService: HeaderService, private cookieService: CookieService, private location: Location){
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(() => {
+      if(this.router.url == '/home' || this.router.url == '/menu'){
+        this.flagGoBackArrow = false;
+      }else{
+        this.flagGoBackArrow = true;
+      }
+    });
+  }
 
   ngOnInit(): void {
     // S'abonner aux mises à jour des données
@@ -27,15 +43,26 @@ export class HeaderComponent implements OnInit {
     }else{
       this.connected = false;
     }
+
+
+    if(this.router.url == '/menu'){
+      this.flagGoBackArrow = false;
+    }
+    console.log(this.flagGoBackArrow);
   }
 
   goToMenu(){
+    this.flagGoBackArrow = false;
     const logo = document.querySelector('#Logo_ARMETISS') as HTMLElement;
     logo.style.transform = 'scale(1.2)';
     setTimeout(() => {
       logo.style.transform = 'scale(1)';
     }, 300);
     this.router.navigate(['/menu']);
+    setTimeout(() => {
+      this.ngOnInit();
+    }, 10);
+
   }
 
   logout(){
@@ -46,4 +73,13 @@ export class HeaderComponent implements OnInit {
     this.headerService.addData(false);
 
   }
+
+  goBack(){
+    if(this.router.url == '/home'){
+      this.flagGoBackArrow = false;
+    }else{
+      this.location.back();
+    }
+  }
+
 }

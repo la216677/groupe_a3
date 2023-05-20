5e7a7911328118a705bc15fbc9a53b60ea91671e
Falconi Franco <100429078+la216677@users.noreply.github.com>
2023-05-20 22:32:48 +0200

    Update updateProductQuantity.php

--- AngularArmetiss/src/server/gestion-stock/updateProductQuantity.php
+++ AngularArmetiss/src/server/gestion-stock/updateProductQuantity.php
@@ -2,78 +2,52 @@
 
 require_once('../database.php');
 
-$sql = " SELECT SUM(Stock_Quantity)FROM Stock WHERE Id_Product = :idProduct";
-$sql2 = " SELECT SUM(Quantity)FROM Sale_Product WHERE Id_Product = :idProduct";
-$sql3 = "UPDATE Product SET Product_Quantity = :stock WHERE `Id_Product`= :idProduct";
+$sql = "SELECT SUM(Stock_Quantity) AS stock FROM Stock WHERE Id_Product = :idProduct";
+$sql2 = "SELECT SUM(Quantity) AS quantity FROM Sale_Product WHERE Id_Product = :idProduct";
+$sql3 = "UPDATE Product SET Product_Quantity = :stock WHERE Id_Product = :idProduct";
 
-$ProductQuantity = 0;
-$idproduct = "";
-$sommeProduct = 0;
+$idproduct = $_POST['idProduct'];
 
+try {
+  $stmt = $pdo->prepare($sql);
+  $stmt->bindParam(':idProduct', $idproduct);
 
-if (isset($_POST['idProduct']) && !empty($_POST['idProduct'])) {
-  $idproduct = trim($_POST['idProduct']);
+  if ($stmt->execute()) {
+    $stock = $stmt->fetchColumn();
 
-  try {
+    $stmt2 = $pdo->prepare($sql2);
+    $stmt2->bindParam(':idProduct', $idproduct);
 
+    if ($stmt2->execute()) {
+      $quantity = $stmt2->fetchColumn();
 
-    $stmt = $pdo->prepare($sql);
+      $stock -= $quantity;
 
-    $stmt->bindValue(':idProduct', $idproduct);
+      $stmt3 = $pdo->prepare($sql3);
+      $stmt3->bindParam(':stock', $stock);
+      $stmt3->bindParam(':idProduct', $idproduct);
 
-    if ($stmt->execute()) {
-      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-      foreach ($result as $row) {
-        $sommeProductString = $row['SUM(Stock_Quantity)'];
-        $sommeProduct = intval($sommeProductString);
-        $data2 = array('message' => 'Valeur de somme product : ' . $sommeProduct);
-
-          echo json_encode($data2);
-      }
-
-      $stmt2 = $pdo->prepare($sql2);
-
-      $stmt2->bindValue(':idProduct', $idproduct);
-
-      if ($stmt2->execute()) {
-        $result = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-
-        foreach ($result as $row) {
-          $ProductQuantityString = $row['SUM(Quantity)'];
-          $ProductQuantity = intval($ProductQuantityString);
-        }
-
-        $stock = $sommeProduct - $ProductQuantity;
-        $stmt3 = $pdo->prepare($sql3);
-
-        $stmt3->bindValue(':stock', $stock);
-        $stmt3->bindValue(':idProduct', $idproduct);
-
-        if ($stmt3->execute()) {
-          $data = array('message' => 'Tout est OK');
-
-          echo json_encode($data);
-          http_response_code(200);
-        } else {
-          $data = array('message' => 'Erreur sql3');
-
-          echo json_encode($data);
-          http_response_code(400);
-        }
+      if ($stmt3->execute()) {
+        $data = array('message' => 'Tout est OK');
+        echo json_encode($data);
+        http_response_code(200);
       } else {
-        $data = array('message' => 'Erreur sql2');
-
+        $data = array('message' => 'Erreur sql3');
         echo json_encode($data);
         http_response_code(400);
       }
     } else {
-      $data = array('message' => 'Erreur sql1');
-
+      $data = array('message' => 'Erreur sql2');
       echo json_encode($data);
       http_response_code(400);
     }
-  } catch (PDOException $e) {
-    http_response_code(404);
+  } else {
+    $data = array('message' => 'Erreur sql1');
+    echo json_encode($data);
+    http_response_code(400);
   }
+} catch (PDOException $e) {
+  $data = array('message' => 'Erreur de la base de données');
+  echo json_encode($data);
+  http_response_code(404);
 }
